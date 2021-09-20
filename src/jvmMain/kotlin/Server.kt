52e5fa382ba2431095c44d1d2d84d316fea7a6b7
Juan Camilo Zorrilla <juanmiloz@hotmail.com>
2021-09-20 00:08:07 -0500

    add code to create a new user

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -87,10 +87,6 @@ fun main() {
                         ContentType.Text.Html
                     )
                 }
-
-
-
-
             }
 
             get("/create") {
@@ -98,6 +94,54 @@ fun main() {
                     this::class.java.classLoader.getResource("create.html")!!.readText(),
                     ContentType.Text.Html
                 )
+            }
+
+            post("/create"){
+                val parameters = call.receiveParameters()
+
+                var username:String = parameters["username"].toString()
+                var password:String = parameters["password"].toString()
+                var confirmPassword:String = parameters["confirmPassword"].toString()
+                var firstName:String = parameters["firstName"].toString()
+                var lastName:String = parameters["lastName"].toString()
+                var birthdate:String = parameters["birthdate"].toString()
+                var exist:Boolean = false
+
+                if(!username.equals("") && !password.equals("") && !confirmPassword.equals("") && !firstName.equals("") && !lastName.equals("") && !birthdate.equals("")){
+                    if(password.equals(confirmPassword)){
+                        for(item in usersList){
+                            if(item.username.equals(username)){
+                                exist = true;
+                            }
+                        }
+                        if(!exist){
+                            var newUser:User = User(firstName, lastName, username, password, confirmPassword, birthdate)
+                            usersList.add(newUser)
+                            call.respondText(
+                                this::class.java.classLoader.getResource("index.html")!!.readText(),
+                                ContentType.Text.Html
+                            )
+                        }else{
+                            print("El usuario ingresado ya existe")
+                            call.respondText(
+                                this::class.java.classLoader.getResource("create.html")!!.readText(),
+                                ContentType.Text.Html
+                            )
+                        }
+                    }else{
+                        println("Las contraseñas no son iguales")
+                        call.respondText(
+                            this::class.java.classLoader.getResource("create.html")!!.readText(),
+                            ContentType.Text.Html
+                        )
+                    }
+                }else{
+                    println("No pueden haber campos vacios")
+                    call.respondText(
+                        this::class.java.classLoader.getResource("create.html")!!.readText(),
+                        ContentType.Text.Html
+                    )
+                }
             }
 
             static("/") {
